88c07fef1cdd7f1c03524d7108a70fa8762fce17
Unknown <sergio_pla_11@hotmail.com>
2019-02-01 20:31:49 +0100

    pytorch test

--- gym_install_test.py
+++ gym_install_test.py
@@ -7,9 +7,9 @@ This is a temporary script file.
 
 import gym #Cargamos libreria de OpenAI Gym
 
-envirotment = gym.make("MountainCar-v0") #Lanzamos una instancia del videojuego d ela montania rusa
+envirotment = gym.make("BipedalWalker-v2") #Lanzamos una instancia del videojuego d ela montania rusa
 envirotment.reset() #Limpiamos y preparamos el entorno para tomar decisione
 for _ in range(2000): #Durante 2000 iteraciones
     envirotment.render() #Pintamos en pantalla la accion
     envirotment.step(envirotment.action_space.sample()) #Tomamos una decision aleatoria del conjunto disponibles
-    
+envirotment.close() #Cerramos la sesion
---
+++ pytorch_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 20:15:32 2019
+
+@author: Sergio
+"""
+
+import torch as tt
+
+t = tt .tensor(2,5)
+print(t)
